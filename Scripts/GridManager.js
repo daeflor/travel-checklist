42c44ad3cd10e9d2bd031a8e8e006259611a39b2
Daniel Flores <danielericflores@hotmail.com>
2017-08-23 01:02:32 +0200

    - Removed most old commented-out code - Removed old, unused grid logic code from checklist.js - Switched to only referencing BootStrap stylesheet

--- Scripts/GridManager.js
+++ Scripts/GridManager.js
@@ -4,8 +4,6 @@ window.GridManager = function()
     var rows = [];
     document.addEventListener('DOMContentLoaded', function(){divGrid = document.getElementById('grid')});
 
-    //console.log('GridManager accessed for first time');
-
     //TODO could possibly set up button events and other elements here
     // function SetupGridElements()
     // {
@@ -68,8 +66,6 @@ window.GridManager = function()
     {
         var divCol = document.createElement('div');
         divCol.className = "col-3"; 
-        //divCol.style.backgroundColor = "grey";
-        //parent.appendChild(divCol);
         AddInputElement("text", divCol, name);
 
         return divCol;
@@ -80,7 +76,6 @@ window.GridManager = function()
         divCol = document.createElement('div');
         divCol.className = "col"; 
         divCol.style.backgroundColor = "bisque";
-        //parent.appendChild(divCol);
         AddInputElement("number", divCol, quantity, "quantity", 0);
 
         return divCol;
@@ -97,16 +92,7 @@ window.GridManager = function()
         divRow.appendChild(CreateQuantityColumn(wearingQuantity));
         divRow.appendChild(CreateQuantityColumn(backpackQuantity));
     
-        //CreateItemColumn(divRow, itemName);
-    
-        // CreateQuantityColumn(divRow, neededQuantity);
-        // CreateQuantityColumn(divRow, packedQuantity);
-        // CreateQuantityColumn(divRow, wearingQuantity);
-        // CreateQuantityColumn(divRow, backpackQuantity);
-    
         divGrid.insertBefore(divRow, divGrid.children[divGrid.childElementCount-3]);    
-
-        //var itemRow = new ItemRow(divRow);
 
         rows.push(new ItemRow(divRow));
     }
@@ -116,10 +102,6 @@ window.GridManager = function()
         {
             return rows.length; 
         },
-        // GetValuesAtRow : function(index)
-        // {
-        //     return rows[index].GetRowValues();
-        // },
         GetAllRowValues : function()
         {
             var rowValues = [];
@@ -128,60 +110,26 @@ window.GridManager = function()
 
             for (var i = 0; i < rows.length; i++)
             {
-                console.log('Row ' + i + ' : ' + rows[i]);
+                //console.log('Row ' + i + ' : ' + rows[i]);
                 rowValues.push(rows[i].GetRowValues());
                 //console.log('Saved the values for row ' + i);
-                console.log('Row ' + i + ' : name = ' + rowValues[i][0]);
+                //console.log('Row ' + i + ' : name = ' + rowValues[i][0]);
             }
 
-            console.log('There are ' + rowValues.length + ' rows getting saved to local storage.');
+            //console.log('There are ' + rowValues.length + ' rows getting saved to local storage.');
             return rowValues;
         },
         AddNewRow : function()
         {
             CreateRow("", 0, 0, 0, 0);
-            
-            // var divRow = document.createElement('div');
-            // divRow.className = "row"; 
-        
-            // CreateItemColumn(divRow, "");
-        
-            // for (var i = 0; i < 4; i++)
-            // {
-            //     CreateQuantityColumn(divRow, 0);
-            // }
-        
-            // this.parentElement.insertBefore(divRow, this.parentElement.children[this.parentElement.childElementCount-3]);    
-
-            // var itemRow = new ItemRow(divRow);
-
-            // rows.push(itemRow);
         },
-        // RecreateRow : function(itemName, neededQuantity, packedQuantity, wearingQuantity, backpackQuantity)
-        // {
-        //     var divRow = document.createElement('div');
-        //     divRow.className = "row"; 
-        
-        //     CreateItemColumn(divRow, itemName);
-        
-        //     CreateQuantityColumn(divRow, neededQuantity);
-        //     CreateQuantityColumn(divRow, packedQuantity);
-        //     CreateQuantityColumn(divRow, wearingQuantity);
-        //     CreateQuantityColumn(divRow, backpackQuantity);
-        
-        //     this.parentElement.insertBefore(divRow, this.parentElement.children[this.parentElement.childElementCount-3]);    
-
-        //     var itemRow = new ItemRow(divRow);
-
-        //     rows.push(itemRow);
-        // },
         RecreateRowsFromStorage : function(rowValues)
         {
-            console.log('Row Values from local storage : ' + rowValues);
-            console.log('There are ' + rowValues.length + ' rows saved in local storage.');
+            //console.log('Row Values from local storage : ' + rowValues);
+            //console.log('There are ' + rowValues.length + ' rows saved in local storage.');
             for (var i = 0; i < rowValues.length; i++)
             {
-                console.log('Row ' + i + ' : name = ' + rowValues[i][0]);
+                //console.log('Row ' + i + ' : name = ' + rowValues[i][0]);
                 CreateRow(rowValues[i][0], rowValues[i][1], rowValues[i][2], rowValues[i][3], rowValues[i][4]);
             }
         }
--- Scripts/checklist.js
+++ Scripts/checklist.js
@@ -1,130 +1,12 @@
 document.addEventListener('DOMContentLoaded', Start);
-//var rowList = [];
 
 function Start()
 {
-    //console.log('Start called');
     document.getElementById('buttonAddRow').onclick = GridManager.AddNewRow;
     document.getElementById('buttonSaveToStorage').onclick = StoreGrid;
     document.getElementById('buttonLoadFromStorage').onclick = LoadGrid;
     //document.getElementById('buttonAddQuantity').onclick = AddQuantity;
     //document.getElementById('buttonReduceQuantity').onclick = ReduceQuantity;
-}
-
-function ItemRow(row, item, needed, packed, wearing)
-{
-    var row = row;
-
-    var itemColumn = row.children[0];
-    var needed = row.children[1].children[0];
-    var packed = row.children[2].children[0];
-    var wearing = row.children[3].children[0];
-    var backpack = row.children[4].children[0];
-
-    needed.oninput = SetItemColumnColor;
-    packed.oninput = SetItemColumnColor;
-    wearing.oninput = SetItemColumnColor;
-    backpack.oninput = SetItemColumnColor;
-
-    // function SetRowColor()
-    // {
-    //     //console.log("needed: " + needed.value + "; packed: " + packed.value + "; wearing: " + wearing.value + "; total prepared: " + totalPrepared);
-    //     if (needed.value == 0)
-    //     {
-    //         row.style.backgroundColor = "grey";
-    //     }
-    //     else if (needed.value == (parseInt(packed.value) + parseInt(wearing.value)))
-    //     {
-    //         row.style.backgroundColor = "green";
-    //     }
-    //     else
-    //     {
-    //         row.style.backgroundColor = "orange";
-    //     }
-    // }
-
-    function SetItemColumnColor()
-    {
-        if (needed.value == 0)
-        {
-            itemColumn.style.backgroundColor = "grey";
-        }
-        else if (needed.value == (parseInt(packed.value) + parseInt(wearing.value) + parseInt(backpack.value)))
-        {
-            itemColumn.style.backgroundColor = "green";
-        }
-        else
-        {
-            itemColumn.style.backgroundColor = "orange";
-        }
-    }
-}
-
-function AddInputElement(type, parent, value, cssClass, min)
-{
-    var input = document.createElement('input');
-    input.type = type;
-    input.value = value;
-    input.className = cssClass;
-    input.min = min;
-    parent.appendChild(input);
-}
-
-function AddRow()
-{
-    var divRow = document.createElement('div');
-    divRow.className = "row"; 
-
-    var divCol = document.createElement('div');
-    divCol.className = "col-3"; 
-    divCol.style.backgroundColor = "grey";
-    divRow.appendChild(divCol);
-    AddInputElement("text", divCol, "");
-
-    for (var i = 0; i < 4; i++)
-    {
-        divCol = document.createElement('div');
-        divCol.className = "col"; 
-        divCol.style.backgroundColor = "bisque";
-        divRow.appendChild(divCol);
-        AddInputElement("number", divCol, 0, "quantity", 0);
-    }
-
-
-
-
-    // var divCol1 = document.createElement('div');
-    // divCol1.className = "col-sm"; 
-
-    // var divCol2 = document.createElement('div');
-    // divCol2.className = "col-sm"; 
-
-    // var divCol3 = document.createElement('div');
-    // divCol3.className = "col-sm"; 
-
-    // divRow.appendChild(divCol0);
-    // divRow.appendChild(divCol1);
-    // divRow.appendChild(divCol2);
-    // divRow.appendChild(divCol3);
-
-    this.parentElement.insertBefore(divRow, this.parentElement.children[this.parentElement.childElementCount-3]);    
-
-    // RowManager.Create(divRow, divCol0, divCol1, divCol2, divCol3);
-
-    // AddInputElement("text", divCol0, "");
-    // AddInputElement("number", divCol1, 0, "quantity", 0);
-    // AddInputElement("number", divCol2, 0, "quantity", 0);
-    // AddInputElement("number", divCol3, 0, "quantity", 0);
-
-    //var itemRow = new ItemRow(divRow, divCol0, divCol1, divCol2, divCol3);
-    var itemRow = new ItemRow(divRow);
-    //rowList.push(itemRow);
-
-    //console.log(this.parentElement);
-    //var encodedHtml = htmlEscape(this.parentElement.innerHTML);
-    //console.log(encodedHtml);
-    //var decodedHtml = htmlUnescape(encodedHtml);
-    //console.log(decodedHtml);
 }
 
 function StoreGrid()
@@ -134,21 +16,16 @@ function StoreGrid()
 
 function LoadGrid()
 {
-    // var rowsFromStorage = LoadValueFromLocalStorage('rowValues');
-    // console.log(rowsFromStorage);
-    // var parsedRows = JSON.parse(rowsFromStorage);
-    // console.log(parsedRows);
     GridManager.RecreateRowsFromStorage(JSON.parse(LoadValueFromLocalStorage('rowValues')));
 }
 
 /*** helper Functions ***/
 
+// TODO change from SessionStorage to LocalStorage when ready
 function SaveNameValuePairToLocalStorage(name, value)
 {
     if (typeof(Storage) !== "undefined") 
     {
-        //sessionStorage.gridHTML = htmlEscape(document.getElementById('grid').innerHTML);
-        //sessionStorage.rowvalues = GridManager.GetAllRowValues();
         sessionStorage.setItem(name, value);
         console.log('Pair added to sesion storage, with name "' + name + '" and value "' + value + '".');
     } 
@@ -158,14 +35,13 @@ function SaveNameValuePairToLocalStorage(name, value)
     }
 }
 
+// TODO change from SessionStorage to LocalStorage when ready
 function LoadValueFromLocalStorage(name)
 {
     if (typeof(Storage) !== "undefined") 
     {
-        //document.getElementById('grid').innerHTML = htmlUnescape(sessionStorage.gridHTML);
         console.log('Request to load value for "' + name +'" from session storage.');        
         return sessionStorage.getItem(name);
-        //GridManager.RecreateRowsFromStorage(sessionStorage.rowvalues);
     } 
     else 
     {
