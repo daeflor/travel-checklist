60c22759c799d50530106539bd0443aee9fd87e2
Daniel Flores <danielericflores@hotmail.com>
2018-08-15 21:16:39 -0700

    Removed some unused code and move some to a Utilities file

--- Scripts/StandardTemplates.js
+++ Scripts/StandardTemplates.js
@@ -1,4 +1,6 @@
-/** Element Creation **/
+//TODO should this whole file be wrapped in an IIFE?
+
+/** Standard Element Creation **/
 
 //TODO could change child parameter to a list of children instead. 
     //Or better, change to a single object param, and one of the properties is a list of children
@@ -55,7 +57,6 @@ function CreateButtonWithIcon(data)
     return buttonElement;
 }
 
-//TODO maybe this SHOULD actually be in a 'helper' or util file... Maybe 'CustomTemplates' (this) vs 'StandardTemplates' (helpers)...?
 //TODO is it possible to always reuse the same popover for editing quantity values? Is that even desired?
 function CreatePopoverToggle(data)
 {    
@@ -134,102 +135,4 @@ function CreateCollapsibleView(data)
     }
 
     return wrapperElement;
-}
-
-//TODO break this file up into different, more specific Utility files
-
-/** Experimental & In Progress **/
-
-function CreateRowSettingsView(id, elements, nameButton, viewExpandedCallback)
-{
-    CreateSettingsView(id, elements, nameButton, 'row', window.GridManager.ToggleActiveSettingsView, viewExpandedCallback);
-}
-
-function CreateListSettingsView(id, elements, nameButton, viewExpandedCallback)
-{
-    CreateSettingsView(id, elements, nameButton, 'list', window.GridManager.ToggleActiveSettingsView, viewExpandedCallback);
-}
-
-/**
- * Creates a Settings View
- * @param {number} id The ID of the List or List Item to which the Settings View belongs
- * @param {array} elements The elements that are part of the Settings View
- * @param {*} nameButton The object (element) that contains/displays the name of the list item (and which may also toggle the settings view)
- * @param {string} parentType The type of parent ('row' or 'list')
- * @param {*} toggleViewFunction The function that should be called when the settings view is toggled
- */
-function CreateSettingsView(id, elements, nameButton, parentType, toggleViewFunction, viewExpandedCallback)
-{
-    //TODO ideally we'd have a good method of re-arranging the rows list and updating all IDs which rely on index as needed
-
-    /* Create Text Area */
-    elements.editNameTextarea = CreateNewElement('textarea', [ ['class',''] ]);
-    elements.editNameTextarea.textContent = nameButton.textContent; 
-
-    /* Create Delete Button */
-    elements.buttonDelete = CreateNewElement(
-        'button', 
-        [ ['id','Delete-'.concat(id)], ['type','button'] ], 
-        CreateNewElement('i', [['class','fa fa-trash']])
-    );
-
-    /* Create Element Wrappers */
-    var divTextareaName = CreateNewElement('div', [ ['class','col-5 divEditName'] ], elements.editNameTextarea);
-    var divButtonDelete = CreateNewElement('div', [ ['class','col-2'] ], elements.buttonDelete);
-    
-    var settingsRowClass = (parentType == 'list') ? 'row divSettingsWrapperRow divListSettingsWrapperRow' : 'row divSettingsWrapperRow';
-
-    //TODO could consider only having to pass custom classes (i.e. the helper function would create element with default classes, and then add on any custom ones passed to it).
-    elements.wrapper  = CreateCollapsibleView({collapsibleId:'SettingsView-'.concat(id), collapsibleClass:'collapse container-fluid divSettingsWrapper', collapsedChildren:[divTextareaName, divButtonDelete], rowClass:settingsRowClass});
-
-    /* Setup Listeners */
-
-    //When the animation to expand the Settings View starts, inform the GridManager to change the Active Settings View
-    $(elements.wrapper).on('show.bs.collapse', function() 
-    {
-        toggleViewFunction(elements.wrapper);
-    });
-
-    //When the animation to expand the Settings View ends, scroll the Settings View into view
-    $(elements.wrapper).on('shown.bs.collapse', function() 
-    {
-        viewExpandedCallback();
-    });
-
-    elements.editNameTextarea.addEventListener('keypress', function(e) 
-    {
-        if(e.keyCode==13)
-        {
-            elements.editNameTextarea.blur();
-        }
-    });
-
-    elements.editNameTextarea.addEventListener('change', function() 
-    {
-        nameButton.textContent = elements.editNameTextarea.value;
-        window.GridManager.GridModified();
-    });
-}
-
-/** Unused **/
-//TODO move these elsewhere
-
-function htmlEscape(str) 
-{
-    return str
-        .replace(/&/g, '&amp;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#39;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;');
-}
-
-function htmlUnescape(str)
-{
-    return str
-        .replace(/&quot;/g, '"')
-        .replace(/&#39;/g, "'")
-        .replace(/&lt;/g, '<')
-        .replace(/&gt;/g, '>')
-        .replace(/&amp;/g, '&');
 }
---
+++ Scripts/Utilities/Utilities.js
@@ -0,0 +1,21 @@
+/** Unused Utilities **/
+
+function htmlEscape(str) 
+{
+    return str
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+}
+
+function htmlUnescape(str)
+{
+    return str
+        .replace(/&quot;/g, '"')
+        .replace(/&#39;/g, "'")
+        .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>')
+        .replace(/&amp;/g, '&');
+}
