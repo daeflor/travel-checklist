dac090d4e5682d2929ebb59f9fa6a63b8c538f75
Daniel Flores <danielericflores@hotmail.com>
2019-06-16 18:29:59 -0700

    Adding unused service worker experiment file

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+console.log("SERVICE WORKER SAYS HI");
+
+var cacheName = 'daeflor-checklist-travel-0.0.11';
+var filesToCache = [];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+  });
